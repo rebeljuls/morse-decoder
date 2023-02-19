9f6413c7bf1bb1a5389e31fe2d6541115ba8a2cb
rebeljuls <rebel.90@mail.ru>
2023-02-19 21:01:53 +0100

    refactor: add code

--- src/index.js
+++ src/index.js
@@ -38,8 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arrmorse = expr.split("");
+    while (arrmorse[0] === '0') arrmorse.shift()
+    while (arrmorse[arrmorse.length - 1] === '0') arrmorse.pop()
+    let newExpr = arrmorse.join('');
+    if (newExpr.indexOf('0') < 0) 
+    return '.';
+ 
+    for (let i = 1; ; i++) {
+    if (newExpr.indexOf(`1${"0".repeat(i)}1`) >= 0 ||
+         newExpr.indexOf(`${"1".repeat(i+1)}`) < 0) {
+             newExpr = newExpr.replace(new RegExp(`0{${i}}`, 'g'), '0')
+             .replace(new RegExp(`1{${i}}`, 'g'), '1' )
+             //.replace(new RegExp(`*{${i}}`, 'g'), '*' )
+             break
+         }
+     }
+     return newExpr
+     .replace(/111/g, '-')
+     .replace(/000/g, ' ')
+     .replace(/1/g, '.')
+     .replace(/0/g, '');
+ 
+ }
+ 
+ function morsedecode(decodemorse) {
+     let morseArray = decodemorse.split(" ");
+     for (let i = 0; i < morseArray; i++) {
+         if (morseArray[i] === "" && morseArray[i+1] === "") {
+             morseArray.splice(i, 2, ' ')
+ 
+         }
+     if (morseArray[i] !== "") {
+         morseArray[i] = MORSE_TABLE[morseArray[i]]
+     }
+ else {
+     morseArray[i] = ' '
+ 
+ }}
+ return morseArray.join('')
+ }
+
 
 module.exports = {
     decode
